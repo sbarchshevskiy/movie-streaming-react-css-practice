2101f341d47a7b98bf04826fbc7b743fc33cfb10
sergey <sbarchshevskiy@gmail.com>
2021-07-13 14:10:48 -0400

    build feature section

---
+++ src/components/featured/Featured.js
@@ -0,0 +1,38 @@
+import './Featured.scss';
+import image from "../../media/girl_sha_002.jpeg";
+import {InfoOutlined, PlayArrow} from "@material-ui/icons";
+import baywatch from "../../media/baywatch.png"
+export default function Featured() {
+  return (
+    <div className="featured">
+      {type && (
+        <div className="category">
+          <span>{type === "movie" ? "Movies" : "Series"}</span>
+          <select name="genre" id="genre">
+            <option>Genre</option>
+          </select>
+        </div>
+      )}
+
+      <img width="100%" src={image} alt="girl-sha"
+      />
+      <div className="info">
+        <img src={baywatch} alt=""
+        />
+        <span className="description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </span>
+        <div className="buttons">
+          <button className="play">
+            <PlayArrow/>
+            <span>Play</span>
+          </button>
+          <button className="more">
+            <InfoOutlined/>
+            <span>Info</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,12 @@
 import './Home.scss'
 import Navbar from "../navigator/Navbar";
+import Featured from "../featured/Featured";
 
 export default function Home() {
   return(
     <div className="home">
       <Navbar/>
+      <Featured/>
     </div>
   )
 }
