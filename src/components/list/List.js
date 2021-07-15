9b2a6a2cf73471805cbd4a12f814746c0620574f
sergey <sbarchshevskiy@gmail.com>
2021-07-15 15:31:35 -0400

    add functionality to slider for right/left movements

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,14 +1,29 @@
 import './List.scss';
 import {ArrowBackIosOutlined, ArrowForwardIosOutlined} from "@material-ui/icons";
 import ListItem from "../listItems/ListItem";
+import {useRef} from "react";
 
 export default function List(){
+
+  const listRef = useRef()
+
+  const handleClick = (direction) => {
+    let distance = listRef.current.getBoundingClientRect().x - 50;
+    if(direction === "right"){
+      listRef.current.style.transform = `translate(${230 + distance}px)`;
+    }
+    if(direction === "left"){
+      listRef.current.style.transform = `translate(${-230 + distance}px)`;
+    }
+
+    console.log('distance:', distance)
+  }
   return(
     <div className="list">
       <span className="list-title">Continue watching</span>
       <div className="wrapper">
-        <ArrowBackIosOutlined className="slider left"/>
-        <div className="container">
+        <ArrowBackIosOutlined className="slider right" onClick={() => handleClick("right")}/>
+        <div className="container" ref={listRef}>
           <ListItem/>
           <ListItem/>
           <ListItem/>
@@ -24,7 +39,7 @@ export default function List(){
           <ListItem/>
           <ListItem/>
         </div>
-        <ArrowForwardIosOutlined className="slider right"/>
+        <ArrowForwardIosOutlined className="slider left" onClick={()=> handleClick("left")}/>
       </div>
     </div>
   )
